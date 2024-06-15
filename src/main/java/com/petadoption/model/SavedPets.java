330335225fc12d33086958f8a927126e86710c73
acz-a <daniel.t@emi-is.com>
2024-06-15 13:59:16 +0300

    removed savedAt column on savedpet

--- src/main/java/com/petadoption/model/SavedPets.java
+++ src/main/java/com/petadoption/model/SavedPets.java
@@ -11,17 +11,12 @@ public class SavedPets implements Serializable {
     @EmbeddedId
     private SavedPetsId id;
 
-    @Column(name = "saved_at", nullable = false)
-    private LocalDateTime savedAt;
-
-    public SavedPets() {
-        this.savedAt = LocalDateTime.now();
-    }
 
     public SavedPets(SavedPetsId id) {
         this.id = id;
-        this.savedAt = LocalDateTime.now();
     }
+
+    public SavedPets() {}
 
     public SavedPetsId getId() {
         return id;
@@ -31,25 +26,17 @@ public class SavedPets implements Serializable {
         this.id = id;
     }
 
-    public LocalDateTime getSavedAt() {
-        return savedAt;
-    }
-
-    public void setSavedAt(LocalDateTime savedAt) {
-        this.savedAt = savedAt;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof SavedPets)) return false;
         SavedPets that = (SavedPets) o;
-        return Objects.equals(getId(), that.getId()) &&
-                Objects.equals(getSavedAt(), that.getSavedAt());
+        return Objects.equals(getId(), that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getSavedAt());
+        return Objects.hash(getId());
     }
 }
